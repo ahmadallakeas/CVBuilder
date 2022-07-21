0923c5ed08896d4c20b29a07c02f92a6104aeb87
Ahmad Al Lakeas <ahmadallakeas@gmail.com>
2022-07-21 11:13:12 +0300

    Fixed titles and packages

--- CV/Pages/SummaryCV.cshtml.cs
+++ CV/Pages/SummaryCV.cshtml.cs
@@ -3,9 +3,10 @@ using CV.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using Syncfusion.HtmlConverter;
-using Syncfusion.Pdf;
+using System.Drawing;
 using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+using Microsoft.AspNetCore.Http.Extensions;
+
 
 namespace CV.Pages
 {
@@ -17,44 +18,58 @@ namespace CV.Pages
         public UserViewModel userViewModel { get; set; }
 
         private readonly CvDbContext _dbContext;
+        private readonly IHostEnvironment hostEnvironment;
 
-
-        public SummaryCVModel(CvDbContext dbContext)
+        public SummaryCVModel(CvDbContext dbContext, IHostEnvironment hostEnvironment)
         {
             this._dbContext = dbContext;
-
-
+            this.hostEnvironment = hostEnvironment;
         }
         public async Task<IActionResult> OnGetAsync()
         {
 
-            User ?user = await _dbContext.User
+            User? user = await _dbContext.User
                 .Include(u => u.Skills).Include(u => u.Nationality)
                 .Where(u => u.UserId == cvId)
                 .FirstOrDefaultAsync();
             if (user == null)
                 return NotFound();
             PopulateViewModel(user);
-
+          
 
             return Page();
         }
-        //public IActionResult OnPostGeneratePDF()
-        //{
-        //   HtmlToPdfConverter converter= new HtmlToPdfConverter();
-        //    WebKitConverterSettings settings= new WebKitConverterSettings();
-        //    settings.WebKitPath = Path.Combine(_hostingEnvironmet.ContentRootPath, "QtBinariesWindows");
-        //    converter.ConverterSettings = settings;
-        //    string s = "<div><form asp-page=\"SummaryCV\" asp-page-handler=\"GeneratePDF\" ><input type = \"Submit\" class=\"form-control\" value=\"GeneratePDF\"/></form></div>";
-        //    PdfDocument document = converter.Convert(s, string.Empty);
-        //    MemoryStream ms = new MemoryStream();
-        //    document.Save(ms);
-        //    document.Close(true);
-        //    ms.Position = 0;
-        //    FileStreamResult result = new FileStreamResult(ms,"application/pdf");
-        //    result.FileDownloadName = "result.pdf";
-        //    return result;
-        //}
+        public void GeneratePDF()
+        {
+            //var Pdf = new GcPdfDocument();
+            //var Page = Pdf.Pages.Add();
+            //var Graphics = Page.Graphics;
+            //Graphics.DrawHtml(System.IO.File.ReadAllText(hostEnvironment.ContentRootPath+"/Pages/Index.cshtml"), 72, 72, new HtmlToPdfFormat(false), out SizeF size);
+            ////Save the PDF Document
+            //Pdf.Save("Invoice.pdf");
+            //var fn = @"webpage.pdf";
+
+            ////Specify the url to be used for PDF conversion
+            //var uri = new Uri(@HttpContext.Request.GetDisplayUrl());
+            ////Create a GcHtmlRenderer instance.
+            //using (var re = new GcHtmlRenderer(uri))
+            //{
+            //    //The PdfSettings instance is created to specify the pdf related settings that will show up in the generated PDF.
+            //    var pdfSettings = new PdfSettings()
+            //    {
+            //        PageRanges = "1-100",
+            //        Margins = new Margins(0.2f), // narrow margins all around
+            //        IgnoreCSSPageSize = false, // try to ensure that we print the pages as we want
+            //        Landscape = false
+            //    };
+
+            //    //Use the RenderToPdf method of RenderingExtension to generate the pdf file
+            //    re.RenderToPdf(fn, pdfSettings);
+
+            //}
+          ;
+        }
+
         protected void PopulateViewModel(User user)
         {
             userViewModel = new UserViewModel();
